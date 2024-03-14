3fd99fed7bffd1fc2f7985f61adf07ca4e39c90c
Sho <shofujiwara505@gmail.com>
2024-03-14 16:22:32 +0900

    ez, now to display it nicely

--- eloquentjavascript/chessboard/chessBoard.js
+++ eloquentjavascript/chessboard/chessBoard.js
@@ -15,29 +15,19 @@ const chessBoard = (x, y) => {
       hashArray.push(positiveHash.join(""));
       positiveHash = [];
     } else {
-      hashArray.push("bayot");
-      // for (let i = 0; i <= y; i++) {
-      //   if (i % 2 == 0) {
-      //     negativeHash.push("#");
-      //   } else {
-      //     negativeHash.push(" ");
-      //   }
-      // }
-      // negativeHash.push(negativeHash);
-      // negativeHash = [];
+      for (let i = 0; i <= y; i++) {
+        if (i % 2 == 0) {
+          negativeHash.push("#");
+        } else {
+          negativeHash.push(" ");
+        }
+      }
+      hashArray.push(negativeHash.join(""));
+      negativeHash = [];
     }
     repTimes += 1;
   }
   return console.log(hashArray);
 };
 
-chessBoard(10, 6);
-
-// for (let i = 0; i <= y; i++) {
-//   let isEven = 0;
-//   if (isEven % 2 == 0) {
-//     hashArray.push(" ");
-//   } else {
-//     hashArray.push("#");
-//   }
-// }
+chessBoard(10, 10);
